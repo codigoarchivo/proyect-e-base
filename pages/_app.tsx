0550a00270fd27a25a2ad240fd56410923364483
Jackson Jose Quintero Angulo <codigoarchivo@gmail.com>
2022-10-05 20:08:53 -0400

    pages/_app.tsx: se agrego CartProvider, UiProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,29 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { CssBaseline, ThemeProvider } from '@mui/material'
+
 import { lightTheme } from '../themes'
+
+import { SWRConfig } from 'swr'
+
+import { CssBaseline, ThemeProvider } from '@mui/material'
+
+import { CartProvider, UiProvider } from '../context'
+
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={lightTheme}>
-      <CssBaseline/>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <SWRConfig value={{
+      fetcher: (resource, init) => fetch(resource, init).then(res => res.json())
+    }}>
+      <CartProvider>
+        <UiProvider>
+          <ThemeProvider theme={lightTheme}>
+            <CssBaseline />
+            <Component {...pageProps} />
+          </ThemeProvider>
+        </UiProvider>
+      </CartProvider>
+    </SWRConfig>
   )
 }
 
