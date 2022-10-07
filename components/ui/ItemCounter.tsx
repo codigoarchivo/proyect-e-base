7337480876ad6bc1f574f0721391b01febc1f66b
Jackson Jose Quintero Angulo <codigoarchivo@gmail.com>
2022-10-07 18:37:52 -0400

    components/ui/ItemCounter.tsx: se agrego conexion del contador con validaciones

--- components/ui/ItemCounter.tsx
+++ components/ui/ItemCounter.tsx
@@ -1,21 +1,44 @@
+import { FC } from "react";
 import Box from "@mui/material/Box";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 
-import  AddCircleOutline  from "@mui/icons-material/AddCircleOutline";
-import  RemoveCircleOutline  from "@mui/icons-material/RemoveCircleOutline";
+import AddCircleOutline from "@mui/icons-material/AddCircleOutline";
+import RemoveCircleOutline from "@mui/icons-material/RemoveCircleOutline";
 
-export const ItemCounter = () => {
+interface Props {
+    currentValue: number;
+    maxValue: number;
+
+    // method
+    updateQuantity: (newValue: number) => void;
+}
+
+export const ItemCounter: FC<Props> = ({ updateQuantity, currentValue, maxValue }) => {
+
+    const addOrRemove = (value: number) => {
+
+        if (value === -1) {
+            if (currentValue === 1) return;
+
+            return updateQuantity(currentValue - 1)
+        }
+
+        if (currentValue >= maxValue) return;
+
+        updateQuantity(currentValue + 1)
+    };
+
     return (
         <Box display='flex' alignItems='center'>
-            <IconButton>
-                <RemoveCircleOutline/>
+            <IconButton onClick={() => addOrRemove(-1)}>
+                <RemoveCircleOutline />
             </IconButton>
             <Typography sx={{ width: 40, textAlign: 'center' }}>
-                1
+                {currentValue}
             </Typography>
-            <IconButton>
-                <AddCircleOutline/>
+            <IconButton onClick={() => addOrRemove(+1)}>
+                <AddCircleOutline />
             </IconButton>
         </Box>
     )
