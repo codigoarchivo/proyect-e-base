bb186cbb708c666a1158d7bc8599e4f6087207bf
Jackson Jose Quintero Angulo <codigoarchivo@gmail.com>
2022-10-08 20:15:01 -0400

    components/cart/CardList.tsx: se agrego  onNewQuantityValue, listo la informacion

--- components/cart/CardList.tsx
+++ components/cart/CardList.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useContext } from 'react';
 
 import NextLink from 'next/link';
 
@@ -16,32 +16,37 @@ import CardMedia from '@mui/material/CardMedia';
 
 import Button from '@mui/material/Button';
 
-import { initialData } from "../../database/products"
-
 import { ItemCounter } from '../ui';
 
-const productsInCard = [
-    initialData.products[0],
-    initialData.products[1],
-    initialData.products[2],
-]
+import { CartContext } from '../../context';
+
+import { ICartProduct } from '../../interfaces';
+
 
 interface Props {
     editable?: boolean,
 }
 
-export const CardList: FC<Props> = ({ editable = false }) => {
+export const CardList: FC<Props> = ({ editable = false }, key) => {
+
+    const { cart, updateCartQuantity, removeCartProduct } = useContext(CartContext)
+
+    const onNewQuantityValue = (product: ICartProduct, onNewQuantityValue: number) => {
+        product.quantity = onNewQuantityValue;
+        updateCartQuantity(product)
+    };
+
     return (
         <>
             {
-                productsInCard.map(product => (
-                    <Grid container key={product.slug} >
+                cart.map(product => (
+                    <Grid container key={product.slug + product.size} >
                         <Grid item xs={3}>
-                            <NextLink href='/product/slug' passHref>
+                            <NextLink href={`/product/${product.slug}`} passHref>
                                 <Link>
                                     <CardActionArea>
                                         <CardMedia
-                                            image={`/products/${product.images[0]}`}
+                                            image={`/products/${product.images}`}
                                             component='img'
                                             sx={{ borderRadius: '5px' }}
                                         />
@@ -52,18 +57,24 @@ export const CardList: FC<Props> = ({ editable = false }) => {
                         <Grid item xs={7}>
                             <Box display={'flex'} flexDirection={'column'}>
                                 <Typography variant='body1'>{product.title}</Typography>
-                                <Typography variant='body1'>Talla <strong>M</strong></Typography>
+                                <Typography variant='body1'>Talla <strong>{product.size}</strong></Typography    >
                                 {
                                     editable
-                                        ? <ItemCounter />
-                                        : <Typography variant='h5'>3 Items</Typography>
+                                        ? (
+                                            <ItemCounter
+                                                currentValue={product.quantity}
+                                                maxValue={10}
+                                                updateQuantity={(value) => onNewQuantityValue(product, value)}
+                                            />
+                                        )
+                                        : <Typography variant='h5'>{product.quantity} {product.quantity > 1 ? 'productos' : 'producto'}</Typography>
                                 }
                             </Box>
                         </Grid>
                         <Grid item xs={2} display='flex' alignItems='center' flexDirection='column'>
                             <Typography variant='subtitle1'>${' '}{product.price}</Typography>
                             {
-                                editable && <Button variant='text' color='secondary'>Remover</Button>
+                                editable && <Button onClick={() => removeCartProduct(product)} variant='text' color='secondary'>Remover</Button>
                             }
 
                         </Grid>
