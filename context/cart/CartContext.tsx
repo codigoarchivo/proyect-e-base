00989a3d42f2865edca9418f2954d53309907938
Jackson Jose Quintero Angulo <codigoarchivo@gmail.com>
2022-10-08 20:12:17 -0400

    context/cart/: se agrego la funcionalidad del cart

--- context/cart/CartContext.tsx
+++ context/cart/CartContext.tsx
@@ -4,8 +4,16 @@ import { ICartProduct } from '../../interfaces';
 
 interface ContextProps {
     cart: ICartProduct[];
+    numberOfItems: number;
+    subTotal: number;
+    tax: number;
+    total: number;
 
     // method
+
+    addProductToCart: (product: ICartProduct) => void;
+    updateCartQuantity: (product: ICartProduct) => void;
+    removeCartProduct: (product: ICartProduct) => void;
 }
 
 export const CartContext = createContext({} as ContextProps);
