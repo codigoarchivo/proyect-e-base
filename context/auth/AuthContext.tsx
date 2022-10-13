0e7ac334cabcc4e1a9ec67ebd8f789f40d641a10
Jackson Jose Quintero Angulo <codigoarchivo@gmail.com>
2022-10-13 19:10:12 -0400

    context/auth/AuthContext.tsx: se muestre de manera global logout en el context

--- context/auth/AuthContext.tsx
+++ context/auth/AuthContext.tsx
@@ -9,6 +9,7 @@ interface ContextProps {
     // method
     loginUser: (email: string, password: string) => Promise<boolean>;
     registerUser: (name: string, email: string, password: string) => Promise<{ hasError: boolean; message?: string; }>
+    logout: () => void;
 }
 
 export const AuthContext = createContext({} as ContextProps);
