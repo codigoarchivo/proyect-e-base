cf259cf5c391cf593921c9cc8e99208b4e5ef489
Jackson Jose Quintero Angulo <codigoarchivo@gmail.com>
2022-11-11 11:14:14 -0400

    context/cart/CartProvider.tsx: agrego api y cookies

--- context/cart/CartProvider.tsx
+++ context/cart/CartProvider.tsx
@@ -1,10 +1,15 @@
 import { FC, useReducer, ReactNode, useEffect } from 'react';
 
+import axios from 'axios';
+
 import Cookie from 'js-cookie';
 
 import { CartContext, cartReducer } from './';
 
-import { ICartProduct } from '../../interfaces';
+import { ICartProduct, IOrder, ShippingAddress } from '../../interfaces';
+
+import { tesloApi } from '../../api';
+
 
 export interface CartState {
     isLoaded: boolean;
@@ -13,7 +18,11 @@ export interface CartState {
     subTotal: number;
     tax: number;
     total: number;
+
+    shippingAddress?: ShippingAddress;
 }
+
+
 
 interface Props {
     children: ReactNode
@@ -26,6 +35,8 @@ const CART_INITIAL_STATE: CartState = {
     subTotal: 0,
     tax: 0,
     total: 0,
+
+    shippingAddress: undefined,
 }
 
 export const CartProvider: FC<Props> = ({ children }) => {
@@ -42,10 +53,27 @@ export const CartProvider: FC<Props> = ({ children }) => {
     }, [])
 
     useEffect(() => {
-        if (state.cart.length !== 0) {
-            Cookie.set('cart', JSON.stringify(state.cart));
-        }
+
+        if (Cookie.get('firstName')) {
+            const shippingAddress = {
+                firstName: Cookie.get('firstName') || '',
+                lastName: Cookie.get('lastName') || '',
+                address: Cookie.get('address') || '',
+                address2: Cookie.get('address2') || '',
+                zip: Cookie.get('zip') || '',
+                city: Cookie.get('city') || '',
+                country: Cookie.get('country') || '',
+                phone: Cookie.get('phone') || '',
+            }
+
+            dispatch({ type: '[Cart] - LoadAddress from Cookies', payload: shippingAddress })
+        };
+    }, []);
+
+    useEffect(() => {
+        Cookie.set('cart', JSON.stringify(state.cart));
     }, [state.cart]);
+
 
     useEffect(() => {
 
@@ -57,7 +85,7 @@ export const CartProvider: FC<Props> = ({ children }) => {
             numberOfItems,
             subTotal,
             tax: subTotal * taxRate,
-            total: subTotal * taxRate + 1,
+            total: subTotal * (taxRate + 1),
         };
 
         dispatch({ type: '[Cart] - Update order summary', payload: orderSumary })
@@ -66,22 +94,22 @@ export const CartProvider: FC<Props> = ({ children }) => {
 
     const addProductToCart = (product: ICartProduct) => {
         const productInCart = state.cart.some(p => p._id === product._id);
-        if (!productInCart) return dispatch({ type: '[Cart] - Update products in cart', payload: [...state.cart, product] });
+        if (!productInCart) return dispatch({ type: '[Cart] - Update products in cart', payload: [...state.cart, product] })
 
         const productInCartButDifferentSize = state.cart.some(p => p._id === product._id && p.size === product.size);
-        if (!productInCartButDifferentSize) return dispatch({ type: '[Cart] - Update products in cart', payload: [...state.cart, product] });
+        if (!productInCartButDifferentSize) return dispatch({ type: '[Cart] - Update products in cart', payload: [...state.cart, product] })
 
+        // Acumular
         const updatedProducts = state.cart.map(p => {
             if (p._id !== product._id) return p;
             if (p.size !== product.size) return p;
 
-            // Actualizar la Cantidad
+            // Actualizar la cantidad
             p.quantity += product.quantity;
-
             return p;
         });
 
-        dispatch({ type: '[Cart] - Update products in cart', payload: [...updatedProducts] });
+        dispatch({ type: '[Cart] - Update products in cart', payload: updatedProducts });
     };
 
     const updateCartQuantity = (product: ICartProduct) => {
@@ -92,6 +120,63 @@ export const CartProvider: FC<Props> = ({ children }) => {
         dispatch({ type: '[Cart] - Remove product in cart', payload: product })
     };
 
+    const updateAddress = (address: ShippingAddress) => {
+        Cookie.set('firstName', address.firstName);
+        Cookie.set('lastName', address.lastName);
+        Cookie.set('address', address.address);
+        Cookie.set('address2', address.address2 || '');
+        Cookie.set('zip', address.zip);
+        Cookie.set('city', address.city);
+        Cookie.set('country', address.country);
+        Cookie.set('phone', address.phone);
+
+        dispatch({ type: '[Cart] - Update Address', payload: address });
+    };
+
+    const createOrder = async (): Promise<{ hasError: boolean; message?: string }> => {
+
+        if (!state.shippingAddress) {
+            throw new Error('No hay direccción de entrega');
+        };
+
+        const body: IOrder = {
+            orderItems: state.cart.map((p: any) => ({
+                ...p,
+                size: p.size!,
+            })),
+            shippingAddress: state.shippingAddress,
+            numberOfItems: state.numberOfItems,
+            subTotal: state.subTotal,
+            tax: state.tax,
+            total: state.total,
+            isPaid: false
+        }
+
+        try {
+            const { data } = await tesloApi.post<IOrder>('/orders', body);
+
+            dispatch({ type: '[Cart] - Order Complete' })
+
+            return {
+                hasError: false,
+                message: data._id
+            }
+
+
+        } catch (error: any) {
+            if (axios.isAxiosError(error)) {
+                return {
+                    hasError: true,
+                    message: error.response?.data.message
+                }
+            }
+            return {
+                hasError: true,
+                message: 'Error no controlado, hable con el administrador'
+            }
+        }
+    };
+
     return (
         <CartContext.Provider value={{
             ...state,
@@ -99,7 +184,11 @@ export const CartProvider: FC<Props> = ({ children }) => {
             // method
             addProductToCart,
             updateCartQuantity,
-            removeCartProduct
+            removeCartProduct,
+            updateAddress,
+
+            // orders
+            createOrder,
         }}>
             {children}
         </CartContext.Provider>
