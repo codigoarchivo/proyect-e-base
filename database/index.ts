b28b3e5198f2835f3a9efe66ae67e99aef533d55
Jackson Jose Quintero Angulo <codigoarchivo@gmail.com>
2022-11-11 11:24:25 -0400

    database/index.ts: export dbOrders y dbUsers

--- database/index.ts
+++ database/index.ts
@@ -1,5 +1,7 @@
 export * as db from './db';
 export * as dbProducts from './dbProducts';
+export * as dbOrders from './dbOrders';
+export * as dbUsers from './dbUser';
 export * from './constants';
 
 export * as seedDatabase from './seed-data';
