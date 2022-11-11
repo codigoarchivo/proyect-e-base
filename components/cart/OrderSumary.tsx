ef7aa58dc9c25b438963924f1c56507f9b01e434
Jackson Jose Quintero Angulo <codigoarchivo@gmail.com>
2022-11-10 20:13:39 -0400

    components/cart/OrderSumary.tsx: si hay ordenres agregue ls ordenes si no lo del context

--- components/cart/OrderSumary.tsx
+++ components/cart/OrderSumary.tsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { FC, useContext } from 'react';
 
 import Grid from '@mui/material/Grid'
 
@@ -8,9 +8,20 @@ import { CartContext } from '../../context';
 
 import { currency } from '../../utils';
 
-export const OrderSumary = () => {
+interface Props {
+    orderValues: {
+        numberOfItems: number;
+        subTotal: number;
+        total: number;
+        tax: number;
+    }
+}
+
+export const OrderSumary: FC<Props> = ({ orderValues }) => {
 
     const { numberOfItems, subTotal, total, tax } = useContext(CartContext);
+
+    const sumaryValues = orderValues ? orderValues : { numberOfItems, subTotal, total, tax }
 
     return (
         <Grid container>
@@ -19,26 +30,26 @@ export const OrderSumary = () => {
 
             </Grid>
             <Grid item xs={6} display='flex' justifyContent='end'>
-                <Typography>{numberOfItems} {numberOfItems > 1 ? 'productos' : 'producto'}</Typography>
+                <Typography>{sumaryValues.numberOfItems} {sumaryValues.numberOfItems > 1 ? 'productos' : 'producto'}</Typography>
             </Grid>
 
             <Grid item xs={6}>
                 <Typography>SubTotal</Typography>
             </Grid>
             <Grid item xs={6} display='flex' justifyContent='end'>
-                <Typography>{currency.format(subTotal)}</Typography>
+                <Typography>{currency.format(sumaryValues.subTotal)}</Typography>
             </Grid>
             <Grid item xs={6}>
                 <Typography>Inpuesto {Number(process.env.NEXT_PUBLIC_TAX_RATE) * 100}%</Typography>
             </Grid>
             <Grid item xs={6} display='flex' justifyContent='end'>
-                <Typography>{currency.format(tax)}</Typography>
+                <Typography>{currency.format(sumaryValues.tax)}</Typography>
             </Grid>
             <Grid item xs={6} mt={5}>
                 <Typography variant='subtitle1'>Total</Typography>
             </Grid>
             <Grid item mt={5} xs={6} display='flex' justifyContent='end'>
-                <Typography variant='subtitle1'>{currency.format(total)}</Typography>
+                <Typography variant='subtitle1'>{currency.format(sumaryValues.total)}</Typography>
             </Grid>
         </Grid>
     )
