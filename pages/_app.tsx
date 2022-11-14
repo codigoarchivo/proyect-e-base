f77f08eb2ff64b04f6bf522343c673b3a386d8cf
Jackson Jose Quintero Angulo <codigoarchivo@gmail.com>
2022-11-14 07:48:54 -0400

    pages/_app.tsx: agregaron nuevos contex

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,31 +1,39 @@
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
 
-import { lightTheme } from '../themes'
+import { SessionProvider } from "next-auth/react"
 
-import { SWRConfig } from 'swr'
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 
-import { CssBaseline, ThemeProvider } from '@mui/material'
+import { lightTheme } from '../themes';
 
-import { AuthProvider, CartProvider, UiProvider } from '../context'
+import { SWRConfig } from 'swr';
 
-import '../styles/globals.css'
+import { CssBaseline, ThemeProvider } from '@mui/material';
+
+import { AuthProvider, CartProvider, UiProvider } from '../context';
+
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <SWRConfig value={{
-      fetcher: (resource, init) => fetch(resource, init).then(res => res.json())
-    }}>
-      <AuthProvider>
-        <CartProvider>
-          <UiProvider>
-            <ThemeProvider theme={lightTheme}>
-              <CssBaseline />
-              <Component {...pageProps} />
-            </ThemeProvider>
-          </UiProvider>
-        </CartProvider>
-      </AuthProvider>
-    </SWRConfig>
+    <SessionProvider>
+      <PayPalScriptProvider options={{ "client-id": process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID || '' }}>
+        <SWRConfig value={{
+          fetcher: (resource, init) => fetch(resource, init).then(res => res.json())
+        }}>
+          <AuthProvider>
+            <CartProvider>
+              <UiProvider>
+                <ThemeProvider theme={lightTheme}>
+                  <CssBaseline />
+                  <Component {...pageProps} />
+                </ThemeProvider>
+              </UiProvider>
+            </CartProvider>
+          </AuthProvider>
+        </SWRConfig>
+      </PayPalScriptProvider>
+    </SessionProvider>
   )
 }
 
